6a9b7269752f4231a0530f0b04bcc218541ce572
Shaunak-123 <sk77monty@gmail.com>
2021-06-11 11:09:15 +0530

    Added to repository

---
+++ HouseOwner/Company/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Company
+{
+    class Employee
+    {
+        string name,address;
+        int age;
+        public void GetData()
+        {
+            Console.WriteLine("Enter the name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the age");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the address");
+            address = Console.ReadLine();
+        }
+
+        public void DispData()
+        {
+            Console.WriteLine("Name is" + name);
+            Console.WriteLine("Age is" + age);
+            Console.WriteLine("Address is" + address);
+        }
+        
+    
+    }
+
+    class Driver : Employee
+    {
+        int salary;
+        string designation;
+
+        public void GetEmployeeData()
+        {
+            Console.WriteLine("Enter salary");
+            salary = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Designation");
+            designation = Console.ReadLine();
+        }
+        public void DisplayEmployeeData()
+        {
+            Console.WriteLine("Salary is" + salary);
+            Console.WriteLine("Designation is" + designation);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver d1 = new Driver();
+            d1.GetData();
+            d1.GetEmployeeData();
+            d1.DispData();
+            d1.DisplayEmployeeData();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HouseOwner/HouseOwner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HouseOwner
+{
+    class Program
+    {
+        public void BedRoom(String nameandcolor)
+        {
+            Console.WriteLine(nameandcolor);
+        }
+
+        public void BedRoom(String Mname, String color)
+        {
+            Console.WriteLine(Mname + " Likes " + color + " Colour ");
+        }
+        static void Main(string[] args)
+        {
+            Program h1 = new Program();
+            h1.BedRoom("Name is Shan and favorite color is Red");
+            h1.BedRoom("Ramu", "Pink");
+            Console.ReadLine();
+        }
+    }
+}
