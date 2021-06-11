49799c56ed0173ddddf9572ac320157c670769b5
Shaunak-123 <sk77monty@gmail.com>
2021-06-11 11:50:54 +0530

    Added

---
+++ Transaction/Amount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amount
+{
+    public delegate double Delegate_Prod(int a,int b);
+    class Program
+    {
+        static double cost(int amount, int servicecharges)
+        {
+            return amount + servicecharges;
+        }
+        static void Main(string[] args)
+        {
+            Delegate_Prod delobj = new Delegate_Prod(cost);
+            Console.WriteLine("Cost of the car is: ");
+            int v1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Charges of the car");
+            int v2 = Int32.Parse(Console.ReadLine());
+            double res = delobj(v1,v2);
+            Console.WriteLine("Total cost is "+res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Transaction/Transaction/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Transaction
+{
+    class Program
+    {
+        interface ITransaction
+        {
+            void showTransaction();
+            double getAmount();
+        }
+
+        public class Transaction1 : ITransaction
+        {
+            private string tcode;
+            private string date;
+            private double amount;
+
+            public Transaction1()
+            {
+                tcode = "";
+                date = "";
+                amount = 0.0;
+            }
+
+            public Transaction1(string c,string d, double a)
+            {
+                tcode = c;
+                date = d;
+                amount = a;
+
+            }
+
+            public double getAmount()
+            {
+                return amount;
+            }
+
+            public void showTransaction()
+            {
+                Console.WriteLine("Transaction "+tcode);
+                Console.WriteLine("Date " + date);
+                Console.WriteLine("Amount " + amount);
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            Transaction1 t1 = new Transaction1("001", "01/09/2018", 234.98);
+            Transaction1 t2 = new Transaction1("002", "01/07/2019", 345.67);
+            t1.showTransaction();
+            t2.showTransaction();
+            Console.ReadKey();
+        }
+    }
+}
